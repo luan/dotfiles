c17335c0b10dd37c3841b802a9d3a011e2c6c929
Luan Santos <git@luan.sh>
2015-07-18 10:08:22 -0700

    cleanup some of the unused files

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,23 +1,17 @@
 #!/usr/bin/env bash
 
-mv $HOME/.bash_profile $HOME/.bash_profile.bak
-mv $HOME/.vimrc.after $HOME/.vimrc.after.bak
-mv $HOME/.dirs $HOME/.dirs.bak
-mv $HOME/.editrc $HOME/.editrc.bak
-mv $HOME/.gemrc $HOME/.gemrc.bak
-mv $HOME/.gitconfig $HOME/.gitconfig.bak
-mv $HOME/.inputrc $HOME/.inputrc.bak
-mv $HOME/.pryrc $HOME/.pryrc.bak
-mv $HOME/.tmux.conf $HOME/.tmux.conf.bak
-mv $HOME/.secrets $HOME/.secrets.bak
+files=" bash_profile
+vimrc.after
+dir_colors
+editrc
+gemrc
+gitconfig
+inputrc
+pryrc
+tmux.conf
+secrets"
 
-ln -s `pwd`/bash_profile $HOME/.bash_profile
-ln -s `pwd`/vimrc.after $HOME/.vimrc.after
-ln -s `pwd`/dirs $HOME/.dirs
-ln -s `pwd`/editrc $HOME/.editrc
-ln -s `pwd`/gemrc $HOME/.gemrc
-ln -s `pwd`/gitconfig $HOME/.gitconfig
-ln -s `pwd`/inputrc $HOME/.inputrc
-ln -s `pwd`/pryrc $HOME/.pryrc
-ln -s `pwd`/tmux.conf $HOME/.tmux.conf
-ln -s `pwd`/secrets $HOME/.secrets
+for file in $files; do
+  mv    $HOME/.$file $HOME/.$file.bak
+  ln -s $PWD/$file   $HOME/.$file
+done
