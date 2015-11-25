41b3725e730d7e29aafb0625c4a529008e8c8790
Luan Santos <cfcluan@gmail.com>
2015-11-25 20:53:45 +0000

    convert fish init to fish

--- omf/init.fish
+++ omf/init.fish
@@ -1,21 +1,21 @@
-#!/usr/bin/env bash
+set -x EDITOR vim
+set -x GIT_EDITOR vim
 
-export EDITOR='vim'
-export GIT_EDITOR='vim'
+set -x GIT_DUET_GLOBAL true
+set -x GIT_DUET_ROTATE_AUTHOR true
 
-export GIT_DUET_GLOBAL=true
-export GIT_DUET_ROTATE_AUTHOR=true
-
-export GOPATH=$HOME/workspace/go
-export PATH=$GOPATH/bin:$PATH
+set -x GOPATH $HOME/workspace/go
+set -x PATH $GOPATH/bin $PATH
 
 bash $HOME/.vim/scripts/base16-shell/base16-monokai.dark.sh
 
-export BOSH_USE_BUNDLER=true
+set -x BOSH_USE_BUNDLER true
 
-export NVIM_TUI_ENABLE_TRUE_COLOR=1
-alias vim=nvim
+set -x NVIM_TUI_ENABLE_TRUE_COLOR 1
+
+function vim  --description 'Alias vim to neovim'
+  command nvim $argv
+end
 
 source /usr/local/share/chruby/chruby.fish
 chruby 2.1.7
-
