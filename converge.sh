d7210a17fd82d0752e881c0f40b39910e13123ec
Luan Santos <cfcluan@gmail.com>
2016-05-15 17:08:37 -0700

    use brewfile

--- converge.sh
+++ converge.sh
@@ -19,18 +19,18 @@ clone() {
   set -e
 }
 
-brew_install() {
-  set +e
-  brew install $@
-  set -e
-}
-
-brew_upinstall() {
-  brew install $@ || brew upgrade $@
-}
+hostname=$1
+if [ -z "$hostname" ]; then
+  echo "usage: ./converge.sh <hostname>"
+  exit 1
+fi
 
 # Enable passwordless sudo
 sudo perl -pi -e 's/%admin\s+ALL=\(ALL\)\s+ALL/%admin ALL=(ALL) NOPASSWD: ALL/' /etc/sudoers
+
+sudo scutil --set LocalHostName $hostname
+sudo scutil --set ComputerName $hostname
+sudo scutil --set HostName $hostname
 
 # Disable Caps Lock (manual step :/)
 
@@ -49,57 +49,19 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 set -e
 
 brew update
-brew_upinstall caskroom/cask/brew-cask
-brew tap caskroom/versions
-brew tap caskroom/fonts
-brew tap pivotal/tap
-brew tap universal-ctags/universal-ctags
-brew tap git-duet/tap
-brew tap nviennot/tmate
-brew tap neovim/neovim
-
-# general dependencies
-brew_upinstall git
+brew tap Homebrew/bundle
+brew bundle
+brew cleanup --force
 
 clone luan/atom-config .atom
 clone luan/vimfiles    .vim
 clone luan/dotfiles    .dotfiles
 
-# apps and configs
-brew cask install karabiner
+# configs
 copy_plist org.pqrs.Karabiner.plist
-
-brew cask install google-chrome
-
-brew cask install iterm2-beta
 copy_plist com.googlecode.iterm2.plist
-
-brew cask install spectacle
 copy_plist com.divisiblebyzero.Spectacle.plist
 
-brew cask install font-meslo-lg-for-powerline
-brew cask install font-fira-mono-for-powerline
-
-brew_upinstall ack ag aria2 rvm loudfoundry-cli direnv fasd fish fzf coreutils
-brew_upinstall bash git-duet htop-osx jq libevent libffi libtool libyaml mercurial
-brew_upinstall ncdu pstree ruby-install tig tmate tmux tree watch wget xz
-
-brew_install node python python3
-
-# formulas that need setup
-brew_install mysql && \
-  ln -sfv /usr/local/opt/mysql/*.plist $HOME/Library/LaunchAgents
-
-brew_install postgres && \
-  ln -sfv /usr/local/opt/postgresql/*.plist $HOME/Library/LaunchAgents
-
-# formulas that need customization
-brew_upinstall vim --with-lua
-brew_install go --with-cc-common
-brew_upinstall macvim --with-lua
-brew_upinstall universal-ctags --HEAD
-
-brew_install neovim --HEAD
 sudo pip3 install neovim
 
 cd $HOME/.dotfiles
@@ -125,19 +87,12 @@ cat <<EOF | sudo tee /etc/shells
 /bin/zsh
 EOF
 
-chsh -s /usr/local/bin/fish
+user=$(whoami)
+sudo chsh -s /usr/local/bin/fish $user
 OMF_CONFIG=$HOME/.dotfiles/omf CI=true fish <(curl -L https://github.com/oh-my-fish/oh-my-fish/raw/master/bin/install) || true
 
-brew cask install alfred
-brew cask install slack
-brew cask install sublime-text3
-brew cask install atom
-
-echo "VritualBox installation may fail if you have VMs running, consider powering them off."
-brew cask install virtualbox
-
-brew cask install vagrant
-brew cask install wraparound
+mkdir -p $HOME/workspace/go
+export GOPATH=$HOME/workspace/go
 
 go get -v -u github.com/vito/boosh
 go get -v -u github.com/tools/godep
@@ -146,19 +101,8 @@ curl -L  -o /tmp/spiff.zip https://github.com/cloudfoundry-incubator/spiff/relea
 mkdir -p $HOME/bin
 unzip -o /tmp/spiff.zip -d $HOME/bin
 
-brew_upinstall awscli
 
-bash <<EOF
-  source /usr/local/opt/chruby/share/chruby/chruby.sh
-  ruby-install ruby 2.1.7 --no-reinstall
-  chruby ruby-2.1.7
-  gem install bosh_cli
-  gem install bundler
-EOF
-
-mkdir -p $HOME/workspace
-mkdir -p $HOME/workspace/concourse-lite
-
-(cd $HOME/workspace/concourse-lite && vagrant init concourse/lite || true )
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+curl -sSL https://get.rvm.io | bash -s stable --ruby --gems=bundler,bosh_cli
 
 exec fish -l
