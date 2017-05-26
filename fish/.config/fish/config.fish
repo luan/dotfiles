efe6b9e143b4416039ef0533cbfb5568478eebfd
Luan Santos <git@luan.sh>
2017-05-26 07:46:59 -0700

    Fisherman for real now

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,1 +1,29 @@
-/home/luan/.config/fish/config.fish
+# editors
+set -gx EDITOR vim
+set -gx GIT_EDITOR vim
+
+# git duet
+set -gx GIT_DUET_GLOBAL true
+set -gx GIT_DUET_ROTATE_AUTHOR true
+
+# golang
+set -gx PATH $PATH /usr/local/go/bin
+set -gx GOPATH $HOME/workspace/go
+set -gx PATH $GOPATH/bin $PATH
+set -gx PATH $HOME/bin $PATH
+
+set grc_wrap_options_ls -F --color
+
+if status --is-interactive
+  if test "$LIGHT_MODE" = "1"
+    bash $HOME/.vim/scripts/base16-shell/scripts/base16-solarized-light.sh
+  else
+    bash $HOME/.vim/scripts/base16-shell/scripts/base16-ashes.sh
+  end
+end
+
+function pullify --description 'adds PRs as remotes'
+  command git config --add remote.origin.fetch '+refs/pull/*/head:refs/remotes/origin/pr/*';
+  command git fetch origin
+end
+
--- install.sh
+++ install.sh
@@ -16,46 +16,46 @@ pac() {
 
 sudo pacman -Syu --needed --noconfirm yajl git expac
 
-if ! which pacaur; then
-  pushd "$(mktemp -d)"
-    git clone https://aur.archlinux.org/cower.git 
-    gpg --recv-keys --keyserver hkp://pgp.mit.edu 1EB2638FF56C0C53
-    (cd cower ; makepkg -i --noconfirm)
-    git clone https://aur.archlinux.org/pacaur.git
-    (cd pacaur && makepkg -i --noconfirm)
-  popd
-fi
-
-pac acpi
-pac alacritty-git
-pac alsa-utils
-pac compton
-pac diff-so-fancy
-pac direnv
-pac dunst
-pac fasd
-pac fish
-pac git-extras
-pac grc
-pac i3-wm
-pac i3blocks-git
-pac i3lock
-pac jq
-pac lastpass-cli
-pac powerline-fonts-git
-pac rofi
-pac stow
-pac sysstat
-pac tmux
-pac ttf-font-awesome
-pac ttf-iosevka
-pac ttf-iosevka-term
-pac ttf-ms-fonts
-pac xautolock
-#pac xclip
-pac xdg-utils
-pac xsel
-
+# if ! which pacaur; then
+#   pushd "$(mktemp -d)"
+#     git clone https://aur.archlinux.org/cower.git 
+#     gpg --recv-keys --keyserver hkp://pgp.mit.edu 1EB2638FF56C0C53
+#     (cd cower ; makepkg -i --noconfirm)
+#     git clone https://aur.archlinux.org/pacaur.git
+#     (cd pacaur && makepkg -i --noconfirm)
+#   popd
+# fi
+# 
+# pac acpi
+# pac alacritty-git
+# pac alsa-utils
+# pac compton
+# pac diff-so-fancy
+# pac direnv
+# pac dunst
+# pac fasd
+# pac fish
+# pac git-extras
+# pac grc
+# pac i3-wm
+# pac i3blocks-git
+# pac i3lock
+# pac jq
+# pac lastpass-cli
+# pac powerline-fonts-git
+# pac rofi
+# pac stow
+# pac sysstat
+# pac tmux
+# pac ttf-font-awesome
+# pac ttf-iosevka
+# pac ttf-iosevka-term
+# pac ttf-ms-fonts
+# pac xautolock
+# pac xclip
+# pac xdg-utils
+# pac xsel
+# 
 stow home
 stow i3
 stow i3blocks
