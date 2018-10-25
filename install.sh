4431abb2a8ae2591c44efd5972e7c89c10c667ca
Luan Santos <git@luan.sh>
2018-10-25 12:41:32 -0700

    update

--- install.sh
+++ install.sh
@@ -10,29 +10,31 @@ clone() {
   set -e
 }
 
-sudo pacman -Syu --needed --noconfirm yajl git expac
-
-if ! which trizen; then
-  pushd "$(mktemp -d)"
-    git clone https://aur.archlinux.org/trizen.git
-    (cd trizen && makepkg -i --noconfirm)
-  popd
-fi
+sudo pacman -Syu --needed --noconfirm git yay base-devel stow
 
 stow -R home
-stow -R x11
-stow -R autorandr
-stow -R i3
-stow -R dunst
-stow -R compton
-stow -R alacritty
-stow -R rofi
-stow -R i3blocks
-stow -R sxiv
+stow -R gnupg
+stow -R ssh
+stow -R yay
+
+# i3 stuff
+# stow -R x11
+# stow -R autorandr
+# stow -R i3
+# stow -R dunst
+# stow -R compton
+# stow -R alacritty
+# stow -R rofi
+# stow -R i3blocks
+# stow -R sxiv
+
+yay -S - < packages.txt
 
 mkdir -p ~/.config
 
 clone luan/nvim  .config/nvim  || true
+stow -R nvim
+
 clone luan/tmuxfiles .config/tmux || true
 
 (cd $HOME/.config/tmux && ./install)
@@ -54,4 +56,4 @@ if [[ "$(getent passwd "$LOGNAME" | cut -d: -f7)" != "$(which zsh)" ]]; then
   sudo chsh -s "$(which zsh)" "$LOGNAME"
 fi
 
-"$dotfiles_dir/scripts/load-state"
+# "$dotfiles_dir/scripts/load-state"
