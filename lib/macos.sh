9b2d67092a5ff1695dd4b5f632480e491fda5b71
Luan Santos <cfcluan@gmail.com>
2023-01-27 15:19:27 -0800

    Update macos.sh

--- lib/macos.sh
+++ lib/macos.sh
@@ -15,6 +15,7 @@ mac_tweaks() {
   defaults write com.apple.dock autohide -bool true
   defaults write com.apple.dock autohide-time-modifier -float 0
   defaults write com.apple.dock autohide-delay -float 0
+  defaults write -g ApplePressAndHoldEnabled -bool false
 }
 
 brew_install() {
