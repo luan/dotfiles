9e9385cbfdffcc06cec3b5ca0e9571cb7fd30422
Luan Santos <github@luan.sh>
2024-10-20 11:11:12 -0700

    feat: basic home-manager

--- nix/flake.nix
+++ nix/flake.nix
@@ -6,12 +6,15 @@
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
     inputs@{
       self,
       nix-darwin,
+      home-manager,
       nixpkgs,
       nix-homebrew,
     }:
@@ -201,15 +204,13 @@
               "vlc"
             ];
           })
-          # nix-homebrew.darwinModules.nix-homebrew
-          # {
-          #   nix-homebrew = {
-          #     enable = true;
-          #     enableRosetta = true;
-          #     user = "luan";
-          #     autoMigrate = true;
-          #   };
-          # }
+          home-manager.darwinModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users."luan" = import ./home.nix;
+            users.users."luan".home = "/Users/luan";
+          }
         ];
       };
 
@@ -227,8 +228,5 @@
           }
         ];
       };
-
-      # Expose the package set, including overlays, for convenience.
-      # darwinPackages = self.darwinConfigurations."mbpro".pkgs;
     };
 }
