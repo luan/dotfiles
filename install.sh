8679a833b1d109e7b3d302245e55de0b34133742
Luan Santos <luan@thebrowser.company>
2025-07-19 11:58:46 -0700

    feat: ghostty with tmux binds

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 require() {
-  dotfiles_dir="$(cd "$(dirname "$0")" && pwd)"
-  source "${dotfiles_dir}/lib/$@"
+	dotfiles_dir="$(cd "$(dirname "$0")" && pwd)"
+	source "${dotfiles_dir}/lib/$@"
 }
 
 require 'common.sh'
@@ -10,25 +10,26 @@ require 'stdout.sh'
 require 'macos.sh'
 
 main() {
-  (
-  cd "$dotfiles_dir"
+	(
+		cd "$dotfiles_dir"
 
-  # if is_macos; then
-  #   dotheader "Setting up stuff on @umacOS"
-  #   brew_install_all "$(cat "${dotfiles_dir}/mac-packages.txt")"
-  #   dotsay "@redBut this isn't a @bMac!@reset Exiting... "
-  #   mac_tweaks
-  # fi
+		if is_macos; then
+			dotheader "Setting up stuff on @umacOS"
+			brew_install_all "$(cat "${dotfiles_dir}/mac-packages.txt")"
+			dotsay "@redBut this isn't a @bMac!@reset Exiting... "
+			mac_tweaks
+		fi
+		exit
 
-  change_shell
-  setup_nvim_config
-  setup_tmux_config
-  setup_gitconfig
-  setup_zshrc
-  stow -R xdg-configs -t "$HOME/.config"
-  stow -R home -t "$HOME"
-  setup_bin
-  )
+		change_shell
+		setup_nvim_config
+		setup_tmux_config
+		setup_gitconfig
+		setup_zshrc
+		stow -R xdg-configs -t "$HOME/.config"
+		stow -R home -t "$HOME"
+		setup_bin
+	)
 }
 
 main
