41a2257513715598a75daa25f9e4993546c024cd
Luan Santos <git@luan.sh>
2025-09-08 08:46:49 -0700

    fix: claude paths

--- xdg-configs/fish/functions/zai.fish
+++ xdg-configs/fish/functions/zai.fish
@@ -3,5 +3,5 @@ function zai --description 'Claude via z.ai'
     set -lx ANTHROPIC_AUTH_TOKEN $Z_API_KEY
     set -lx ANTHROPIC_MODEL glm-4.5
     set -lx ANTHROPIC_SMALL_FAST_MODEL glm-4.5-air
-    $HOME/.claude/local/claude $argv
+    claude $argv
 end
